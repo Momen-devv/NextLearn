3365c95cd1351e7be63825df39324a7a4470459a
Momen <momen.devv@gmail.com>
2025-08-22 11:35:00 +0300

    create auth and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '../db/data-source';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -30,6 +32,8 @@ import { dataSourceOptions } from '../db/data-source';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(dataSourceOptions),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [
