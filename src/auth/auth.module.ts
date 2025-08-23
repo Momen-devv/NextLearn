15b4f9fa95a5142815a0986b247044f72c5187a3
Momen <momen.devv@gmail.com>
2025-08-23 18:00:21 +0300

    feat(auth): implement user registration logic with verification code generation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          global: true,
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: config.get<string>('JWT_EXPIRES_IN') },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { Repository } from 'typeorm';
+import { hash } from 'bcrypt';
+import { randomBytes } from 'crypto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(dto: RegisterDto) {
+    // Check if user email exist in db user
+    await this.ensureUserNotExists(dto.email);
+
+    // Hash password
+    const hashedPassword = await this.hashPassword(dto.password);
+
+    // Genrate verification code
+    const verificationCode = this.generateVerificationCode();
+
+    // Add user in the db
+    const user = await this.createUser({
+      ...dto,
+      password: hashedPassword,
+      verificationCode,
+    });
+    // Create jwt for user
+    const token = await this.generateToken(user.id, user.role);
+
+    return token;
+
+    // Send email to verify account with verification code
+  }
+
+  // GET /auth/verify-email/:userId/:verificationCode
+  // verify(){}
+
+  private async ensureUserNotExists(email: string) {
+    const user = await this.usersRepository.findOne({ where: { email } });
+    if (user) throw new BadRequestException('User already exists');
+  }
+
+  private async createUser(data: Partial<User>): Promise<User> {
+    const user = this.usersRepository.create(data);
+    return await this.usersRepository.save(user);
+  }
+
+  private hashPassword(password: string): Promise<string> {
+    return hash(password, 10);
+  }
+
+  private generateToken(userId: number, userRole: string): Promise<string> {
+    return this.jwtService.signAsync({ userId, userRole });
+  }
+
+  private generateVerificationCode(): string {
+    return randomBytes(32).toString('hex');
+  }
+}
