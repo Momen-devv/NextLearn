51bc86f2ff37843df88af6fe1e8bc01f124385e2
Momen <momen.devv@gmail.com>
2025-09-02 22:55:30 +0300

    feat(sessions): add cleanup-sessions endpoint to remove old or invalid sessions

--- src/sessions/sessions.controller.ts
+++ src/sessions/sessions.controller.ts
@@ -1,5 +1,7 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   Param,
@@ -11,13 +13,17 @@ import {
 } from '@nestjs/common';
 import type { Request, Response } from 'express';
 import { SessionsService } from './sessions.service';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserRole } from 'src/enums/user-role.enum';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { CleanupSessionDto } from './dto/cleanup-session.dto';
 
 @Controller('sessions')
 export class SessionsController {
   constructor(private sessionsService: SessionsService) {}
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   @HttpCode(200)
   sessions(@Req() req: Request) {
@@ -30,14 +36,14 @@ export class SessionsController {
     return this.sessionsService.refresh(req, res);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard('jwt'))
   @Post('logout')
   @HttpCode(200)
   logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     return this.sessionsService.logout(req, res);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard('jwt'))
   @Post('revoke-session/:sessionId')
   @HttpCode(200)
   revokeSession(
@@ -47,10 +53,17 @@ export class SessionsController {
     return this.sessionsService.revokeSession(sessionId, req);
   }
 
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard('jwt'))
   @Post('revoke-all-sessions')
   @HttpCode(200)
   revokeAllSessions(@Req() req: Request) {
     return this.sessionsService.revokeAllSessions(req);
+  }
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Delete('cleanup-sessions')
+  cleanupSessions(@Body() dto: CleanupSessionDto) {
+    return this.sessionsService.cleanupSessions(dto);
   }
 }
